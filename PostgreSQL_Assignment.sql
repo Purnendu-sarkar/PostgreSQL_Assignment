07817eb1e346ae4bd8f90d7a3b3708b211317ecd
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-05-26 15:36:08 +0600

    Solve Problem 2: Count unique species ever sighted

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -120,3 +120,7 @@ VALUES (
 INSERT INTO
     rangers (name, region)
 VALUES ('Derek Fox', 'Coastal Plains');
+
+-- Problem 2: Count unique species ever sighted
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
