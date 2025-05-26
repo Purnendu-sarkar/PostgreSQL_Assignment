784c8b5d2b4271db5001c62fd2af7e3a306d85c7
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-05-26 14:53:00 +0600

    Create tables: rangers, species, and sightings with necessary constraints

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,33 @@
 CREATE DATABASE conservation_db;
+
+-- Create rangers table
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
+
+-- Create species table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL CHECK (
+        conservation_status IN (
+            'Endangered',
+            'Vulnerable',
+            'Historic'
+        )
+    )
+);
+
+-- Create sightings table
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INTEGER NOT NULL REFERENCES rangers (ranger_id) ON DELETE CASCADE,
+    species_id INTEGER NOT NULL REFERENCES species (species_id) ON DELETE CASCADE,
+    sighting_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    location VARCHAR(100) NOT NULL,
+    notes TEXT
+);
